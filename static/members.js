a22f9788ad98d2795a9529cdb92020e74bffa9f3
大空スバル大好き <111629702+BlueBoy247@users.noreply.github.com>
2024-08-04 01:41:20 +0800

    initial commit

---
+++ static/members.js
@@ -0,0 +1,26 @@
+function i(){
+    alert("nope");
+
+}
+
+
+function m(){
+    var m = document.getElementById("m");
+    //var n = m.style.top;
+    //n = n.replace("px","");
+    window.scrollTo(0,470);
+}
+
+function up(){
+    var m = document.getElementById("m");
+    //var n = m.style.top;
+    //n = n.replace("px","");
+    window.scrollTo(0,0);
+}
+
+function e(){
+    var m = document.getElementById("m");
+    //var n = m.style.top;
+    //n = n.replace("px","");
+    window.scrollTo(0,5140);
+}
